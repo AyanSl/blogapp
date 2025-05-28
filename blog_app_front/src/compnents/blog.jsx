50ddfbd2cb1aad625c5f765a7af0975f148b4225
AyanSI <ayanshahlaravel343546@gmail.com>
2025-05-28 20:06:11 +0500

    code commit

--- blog_app_front/src/compnents/blog.jsx
+++ blog_app_front/src/compnents/blog.jsx
@@ -4,12 +4,12 @@ import Blogcard from "./blogcard"
 const blog = () => {
 
   const [blogs , setblogs] = useState();
+  const [keyword , setkeyword] = useState('');
 
 
   const FetchBlogs = async () =>{
     const res =  await  fetch('http://127.0.0.1:8000/api/blogs');
-    const result = await res.json()
-    console.log(result.data);
+    const result = await res.json();
     setblogs(result.data);
   }
 
@@ -17,9 +17,33 @@ const blog = () => {
     FetchBlogs();
   },[])
 
+  const formserchsubmit = async (event) =>{
+    event.preventDefault();
+    const res =  await  fetch('http://127.0.0.1:8000/api/blogs?keyword='+keyword);
+    const result = await res.json();
+    console.log(result.data);
+    setblogs(result.data);
+  }
+
+  const RestForm = () => {
+    FetchBlogs();
+    setkeyword('');
+  }
+
   return (
     <>
       <div className="container">
+        
+        <div className="d-flex justify-content-center pt-5 mb-4">
+            <form onSubmit={(e) => formserchsubmit(e)}>
+          <div className='d-flex'>
+            <input type="text" placeholder='Enter a keyword' value={keyword} onChange={(e) => setkeyword(e.target.value)} className='form-control' />
+            <button className="btn btn-dark ms-2">Search</button>
+            <button className="btn btn-info ms-2" onClick={(e) => RestForm(e)}>Reset</button>
+          </div>
+            </form>
+        </div>
+
         <div className="d-flex justify-content-between pt-5 mb-4">
           <h4>Blogs</h4>
           <a href="/create" className="btn btn-dark">Create</a>
